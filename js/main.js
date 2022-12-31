e43d2f44356855372a0f4a76346233a85c54a2ba
Dogdich <malugamikola@gmail.com>
2022-12-31 18:04:26 +0200

    add

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+
+"use strict"
+
+document.addEventListener('click', documentClick);
+
+function documentClick(e){
+    const targetItem = e.target;
+    if(targetItem.closest('.icon-menu')){
+       document.documentElement.classList.toggle('menu-open'); 
+    }
+
+    
+}
+    $(document).ready(function () {
+      $("a").on("click", function (event) {
+        if (this.hash !== "") {
+          event.preventDefault();
+          var hash = this.hash;
+          $("html, body").animate(
+            {
+              scrollTop: $(hash).offset().top,
+            },
+            800,
+            function () {
+              window.location.hash = hash;
+            }
+          );
+        } 
+      });
+    });
